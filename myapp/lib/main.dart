9ec167a6461079c1615eebabea42ee1f0fa78025
Thiago Cavalcante <thiago.kun@gmail.com>
2020-05-16 20:10:59 -0300

    Video #12b

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -16,9 +16,15 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text('Hello'),
+              Text('World'),
+            ],
+          ),
           Container(
               padding: EdgeInsets.all(20.0),
               color: Colors.cyan,
