3767ada7f0bfbe5f7a479d5dce16f0ef49bf3525
Thiago Cavalcante <thiago.kun@gmail.com>
2020-05-16 20:10:24 -0300

    Video #10b

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -15,10 +15,8 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Container(
-        padding: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
-        margin: EdgeInsets.all(30.0),
-        color: Colors.grey[400],
+      body: Padding(
+        padding: EdgeInsets.all(90.0),
         child: Text('Hello'),
       ),
       floatingActionButton: FloatingActionButton(
