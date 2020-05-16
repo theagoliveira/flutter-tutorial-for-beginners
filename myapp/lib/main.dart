b10f9e389858b97a1a59e5767d47db9030890123
Thiago Cavalcante <thiago.kun@gmail.com>
2020-05-16 20:07:09 -0300

    Video #05

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,3 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Text("hey, ninjas!")));
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("Hello, ninjas!"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Text("Click"),
+      ),
+    ),
+  ));
+}
