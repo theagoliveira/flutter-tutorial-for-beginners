1252162b8ef78625e08fb90101abfb998a24f43f
Thiago Cavalcante <thiago.kun@gmail.com>
2020-05-16 21:02:26 -0300

    Video #21

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -43,7 +43,16 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
+        children: quotes
+            .map((quote) => QuoteCard(
+                  quote: quote,
+                  delete: () {
+                    setState(() {
+                      quotes.remove(quote);
+                    });
+                  },
+                ))
+            .toList(),
       ),
     );
   }
